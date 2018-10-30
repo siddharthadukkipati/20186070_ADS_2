56f72e7ae8a96a97e6ee06e59ed64a7e2c4329f4
Siddhartha Dukkipati <siddharthadukipati@msitprogram.net>
2018-10-30 20:06:46 +0530

    "ads2_m2_a1_1: 24/24 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ Assignments/M2/2.1 CC_Percolation/Solution.java
@@ -0,0 +1,170 @@
+/**
+ * percolation theory.
+ */
+import java.util.Scanner;
+/**
+ * Class for percolation.
+ */
+class Percolation {
+    /**
+     * declaration of graph.
+     */
+    private Graph graph;
+    /**
+     * private declaration of n.
+     */
+    private int n;
+    /**
+     * declaration of size.
+     */
+    private int size;
+    /**
+     * declaration of top.
+     */
+    private int top;
+    /**
+     * declaration of bottom.
+     */
+    private int bottom;
+    /**
+     * declaration of count.
+     */
+    private int count;
+    /**
+     * declaration of boolean array.
+     */
+    private boolean[] connected;
+    /**
+     * Constructs the object.
+     *
+     * @param      n1    The n 1
+     */
+    Percolation(final int n1) {
+        this.n = n1;
+        this.size = n1 * n1;
+        this.top = size;
+        this.bottom = size + 1;
+        this.count = 0;
+        graph = new Graph(size + 2);
+        connected = new boolean[size];
+        for (int i = 0; i < n; i++) {
+            graph.addEdge(top, i);
+            graph.addEdge(bottom, size - i - 1);
+        }
+    }
+    /**
+     * converts to 1D array.
+     *
+     * @param      i     { parameter_description }
+     * @param      j     { parameter_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
+    private int convert(final int i, final int j) {
+//since we take size as 0 to n-1, we decrement one value from rows and columns.
+        return n * (i - 1) + (j - 1);
+    }
+    /**
+     * Connects open sites(== full site).
+     *
+     * @param      row   The row
+     * @param      col   The col
+     */
+    private void connectOpenSites(final int row, final int col) {
+        if (connected[col] && !graph.hasEdge(row, col)) {
+            graph.addEdge(row, col);
+        }
+    }
+    /**
+     * opens the blocked sites.
+     *
+     * @param      row   The row
+     * @param      col   The col
+     */
+    public void open(final int row, final int col) {
+        // int count = 0;
+        int index = convert(row, col);
+        connected[index] = true;
+        count++;
+        int toprow = index - n;
+        int bottomrow = index + n;
+        if (n == 1) {
+            graph.addEdge(top, index);
+            graph.addEdge(bottom, index);
+        }
+        if (bottomrow < size) { //bottom
+            connectOpenSites(index, bottomrow);
+        }
+        if (toprow >= 0) { //top
+            connectOpenSites(index, toprow);
+        }
+        if (col == 1) { //left
+            if (col != n) {
+                connectOpenSites(index, index + 1);
+            }
+            return;
+        }
+        if (col == n) { //right
+            connectOpenSites(index, index - 1);
+            return;
+        }
+        connectOpenSites(index, index + 1);
+        connectOpenSites(index, index - 1);
+    }
+    /**
+     * Determines if it is an open site.
+     *
+     * @param      row   The row
+     * @param      col   The col
+     *
+     * @return     True if open, False otherwise.
+     */
+    public boolean isOpen(final int row, final int col) {
+        return connected[convert(row, col)];
+    }
+    /**
+     * counts number of open sites.
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public int numberOfOpenSites() {
+        return count;
+    }
+    /**
+     * returns true if percolates.
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public boolean percolates() {
+        CC connectedComponents = new CC(graph);
+        return connectedComponents.connected(top, bottom);
+    }
+}
+/**
+ * class for solution.
+ */
+public final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        //not used
+    }
+    /**
+     * main function.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        // Graph graph = new Graph(n);
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+        Percolation p = new Percolation(n);
+        while (sc.hasNext()) {
+            String[] tokens = sc.nextLine().split(" ");
+            p.open(Integer.parseInt(tokens[0]), Integer.parseInt(tokens[1]));
+        }
+        System.out.println(p.percolates() && p.numberOfOpenSites() != 0);
+    }
+}
+
---
+++ Assignments/M2/2.1 CC_Percolation/SolutionPP.java
@@ -0,0 +1,188 @@
+// public class Percolation {
+//    public Percolation(int n)
+// create n-by-n grid, with all sites blocked
+//    public    void open(int row, int col)
+// open site (row, col) if it is not open already
+//    public boolean isOpen(int row, int col)
+// is site (row, col) open?
+//    public boolean isFull(int row, int col)
+// is site (row, col) full?
+//    public     int numberOfOpenSites()
+// number of open sites
+//    public boolean percolates()
+// does the system percolate?
+// }
+
+
+// You can implement the above API to solve the problem
+import java.util.Scanner;
+/**
+ * Class for percolation.
+ */
+class Percolation {
+    /**
+     *the array.
+     */
+    private boolean[] array;
+    /**
+     *object declaration for class.
+     */
+    private Graph graph;
+    /**
+     *array size.
+     */
+    private int arraySize;
+    /**
+     *size.
+     */
+    private int size;
+    /**
+     * initializing count.
+     */
+    private int count;
+    /**
+     *first row.
+     */
+    private int top;
+    /**
+     * last row.
+     */
+    private int bottom;
+    /**
+     * Constructs the object.
+     */
+    protected Percolation() {
+
+    }
+    /**
+     * Constructs the object.
+     *
+     * @param n int
+     */
+    Percolation(final int n) {
+        this.arraySize = n;
+        this.size = n * n;
+        this.top = size;
+        this.bottom = size + 1;
+        this.count = 0;
+        graph = new Graph(size + 2);
+        array = new boolean[size];
+        for (int i = 0; i < arraySize; i++) {
+            graph.addEdge(top, i);
+            graph.addEdge(bottom, size - i - 1);
+        }
+    }
+    /**
+     * method to convert from two dimensional to one dimensional.
+     *
+     * @param      row   The row
+     * @param      col   The col
+     *
+     * @return  onedimensional array
+     */
+    public int toOneD(final int row, final int col) {
+        return (arraySize * (row - 1)) + (col - 1);
+    }
+    /**
+     * Connects open sites(== full site).
+     *
+     * @param      row   The row
+     * @param      col   The col
+     */
+    private void connectOpenSites(final int row, final int col) {
+        if (array[col] && !graph.hasEdge(row, col)) {
+            graph.addEdge(row, col);
+        }
+    }
+    /**
+     * method that opens the blocked site.
+     *
+     * @param      row     The row
+     * @param      col  The column
+     */
+    public void open(final int row, final int col) {
+        int index = toOneD(row, col);
+        array[index] = true;
+        count++;
+        int toprow = index - arraySize;
+        int bottomrow = index + arraySize;
+        if (arraySize == 1) {
+            graph.addEdge(top, index);
+            graph.addEdge(bottom, index);
+        }
+        if (bottomrow < size) {         //bottom
+            connectOpenSites(index, bottomrow);
+        }
+        if (toprow >= 0) {              //top
+            connectOpenSites(index, toprow);
+        }
+        if (col == 1) {                 //left
+            if (col != arraySize) {
+                connectOpenSites(index, index + 1);
+            }
+            return;
+        }
+        if (col == arraySize) {         //right
+            connectOpenSites(index, index - 1);
+            return;
+        }
+        connectOpenSites(index, index + 1);
+        connectOpenSites(index, index - 1);
+    }
+    /**
+     * Determines if open.
+     *
+     * @param      row   The row
+     * @param      col   The col
+     *
+     * @return     True if open, False otherwise.
+     */
+    public boolean isOpen(final int row, final int col) {
+        return array[toOneD(row, col)];
+    }
+    /**
+     * return number of open sites.
+     *
+     * @return count
+     */
+    public int numberOfOpenSites() {
+        return count;
+    }
+    /**
+     * method to check whether there is a flow.
+     *
+     * @return boolean
+     */
+    public boolean percolates() {
+        CC connectedComponents = new CC(graph);
+        return connectedComponents.connected(top, bottom);
+    }
+}
+/**
+ * Class for solution.
+ */
+public final class Solution {
+    /**
+     * Constructs the object.
+     */
+    protected Solution() {
+
+    }
+    /**
+     * main method to read input.
+     *
+     * @param args String
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = Integer.parseInt(scan.nextLine());
+        Percolation pobj = new Percolation(n);
+        while (scan.hasNext()) {
+            String[] tokens = scan.nextLine().split(" ");
+            pobj.open(Integer.parseInt(tokens[0]),
+                      Integer.parseInt(tokens[1]));
+        }
+        System.out.println(pobj.percolates()
+                           && pobj.numberOfOpenSites() != 0);
+    }
+}
