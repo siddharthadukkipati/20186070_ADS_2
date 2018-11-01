9d0dc58b92e2a3b447e3b22e7a00fcd6da8dda2f
Siddhartha Dukkipati <siddharthadukipati@msitprogram.net>
2018-11-01 17:12:21 +0530

    "ads2_m3_a2_1: 0/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Assignments/M3/3.2 Bipartite/Graph.java
+++ Assignments/M3/3.2 Bipartite/Graph.java
@@ -1,153 +1,185 @@
-/*************************************************************************
- *  Compilation:  javac Graph.java        
- *  Execution:    java Graph input.txt
- *  Dependencies: Bag.java In.java StdOut.java
- *  Data files:   http://algs4.cs.princeton.edu/41undirected/tinyG.txt
- *
- *  A graph, implemented using an array of sets.
- *  Parallel edges and self-loops allowed.
- *
- *  % java Graph tinyG.txt
- *  13 vertices, 13 edges 
- *  0: 6 2 1 5 
- *  1: 0 
- *  2: 0 
- *  3: 5 4 
- *  4: 5 6 3 
- *  5: 3 4 0 
- *  6: 0 4 
- *  7: 8 
- *  8: 7 
- *  9: 11 10 12 
- *  10: 9 
- *  11: 9 12 
- *  12: 11 9 
- *
- *  % java Graph mediumG.txt
- *  250 vertices, 1273 edges 
- *  0: 225 222 211 209 204 202 191 176 163 160 149 114 97 80 68 59 58 49 44 24 15 
- *  1: 220 203 200 194 189 164 150 130 107 72 
- *  2: 141 110 108 86 79 51 42 18 14 
- *  ...
- *  
- *************************************************************************/
-
-
+import java.util.Iterator;
+import java.util.NoSuchElementException;
 /**
- *  The <tt>Graph</tt> class represents an undirected graph of vertices
- *  named 0 through V-1.
- *  It supports the following operations: add an edge to the graph,
- *  iterate over all of the neighbors adjacent to a vertex.
- *  Parallel edges and self-loops are permitted.
- *  <p>
- *  For additional documentation, see <a href="http://algs4.cs.princeton.edu/51undirected">Section 5.1</a> of
- *  <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
+ * Class for graph.
  */
 public class Graph {
-    private final int V;
-    private int E;
-    private Bag<Integer>[] adj;
-    
-   /**
-     * Create an empty graph with V vertices.
+    /**
+     *the variable to maintain vertex.
      */
-    public Graph(int V) {
-        if (V < 0) throw new RuntimeException("Number of vertices must be nonnegative");
-        this.V = V;
-        this.E = 0;
-        adj = (Bag<Integer>[]) new Bag[V];
-        for (int v = 0; v < V; v++) {
-            adj[v] = new Bag<Integer>();
+    private final int vertices;
+    /**
+     *the variable to maintain edges.
+     */
+    private int edges;
+    /**
+     *the variable to adjacency matrix.
+     */
+    private boolean[][] adj;
+    /**
+     *the constructor to initialize.
+     *
+     * @param      vertices1  vertices
+     */
+    public Graph(final int vertices1) {
+        if (vertices1 < 0) {
+            throw new IllegalArgumentException("Too few vertices");
         }
+        this.vertices = vertices1;
+        this.edges = 0;
+        this.adj = new boolean[vertices1][vertices1];
+    }
+    /**
+     *
+     *the method to return vertices.
+     * @return vertices.
+     */
+    public int vertices() {
+        return vertices;
+    }
+    /**
+     *the method is to return edges.
+     *
+     * @return  edges.
+     */
+    public int edges() {
+        return edges;
+    }
+    /**
+     * adds an edge.
+     *
+     * @param      v  vertexOne
+     * @param      w  vertexTwo
+     */
+    public void addEdge(final int v, final int w) {
+        if (!adj[v][w]) {
+            edges++;
+        }
+        adj[v][w] = true;
+        adj[w][v] = true;
+    }
+    /**
+     *the method returns whether there is.
+     *a connection between two vertices.
+     * @param      v     { parameter_description }
+     * @param      w     { parameter_description }
+     *
+     * @return connection is there are not.
+     */
+
+    /**
+     * checks if contains.
+     *
+     * @param      v     { parameter_description }
+     * @param      w     { parameter_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public boolean contains(final int v, final int w) {
+        return adj[v][w];
     }
 
-   /**
-     * Create a random graph with V vertices and E edges.
-     * Expected running time is proportional to V + E.
+    /**
+     * return list of neighbors of v.
+     *
+     * @param      v  vertex
+     *
+     * @return  iterator
      */
-    public Graph(int V, int E) {
-        this(V);
-        if (E < 0) throw new RuntimeException("Number of edges must be nonnegative");
-        for (int i = 0; i < E; i++) {
-            int v = (int) (Math.random() * V);
-            int w = (int) (Math.random() * V);
-            addEdge(v, w);
-        }
+    public Iterable<Integer> adj(final int v) {
+        return new AdjIterator(v);
     }
-
-   /**  
-     * Create a digraph from input stream.
+    /**
+     *support iteration over graph vertices.
      */
-/*    public Graph(In in) {
-        this(in.readInt());
-        int E = in.readInt();
-        for (int i = 0; i < E; i++) {
-            int v = in.readInt();
-            int w = in.readInt();
-            addEdge(v, w);
+    private class AdjIterator implements Iterator<Integer>,
+        Iterable<Integer> {
+        /**
+         * v.
+         */
+        private int v;
+        /**
+         * w.
+         */
+        private int w = 0;
+
+        /**
+         * Constructs the object.
+         *
+         * @param      v1    The v 1
+         */
+        AdjIterator(final int v1) {
+            this.v = v1;
         }
-    }*/
 
-   /**
-     * Copy constructor.
-     */
-/*    public Graph(Graph G) {
-        this(G.V());
-        this.E = G.E();
-        for (int v = 0; v < G.V(); v++) {
-            // reverse so that adjacency list is in same order as original
-            Stack<Integer> reverse = new Stack<Integer>();
-            for (int w : G.adj[v]) {
-                reverse.push(w);
+        /**
+         * iterator.
+         *
+         * @return     { description_of_the_return_value }
+         */
+        public Iterator<Integer> iterator() {
+            return this;
+        }
+
+        /**
+         * Determines if it has next.
+         *
+         * @return     True if has next, False otherwise.
+         */
+        public boolean hasNext() {
+            while (w < vertices) {
+                if (adj[v][w]) {
+                    return true;
+                }
+                w++;
             }
-            for (int w : reverse) {
-                adj[v].add(w);
-            }
+            return false;
         }
-    }*/
 
-   /**
-     * Return the number of vertices in the graph.
-     */
-    public int V() { return V; }
+        /**
+         * next.
+         *
+         * @return     { description_of_the_return_value }
+         */
+        public Integer next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            return w++;
+        }
 
-   /**
-     * Return the number of edges in the graph.
-     */
-    public int E() { return E; }
-
-
-   /**
-     * Add the edge v-w to graph.
-     */
-    public void addEdge(int v, int w) {
-        E++;
-        adj[v].add(w);
-        adj[w].add(v);
+        /**
+         * remove.
+         */
+        public void remove()  {
+            throw new UnsupportedOperationException();
+        }
     }
-
-
-   /**
-     * Return the list of neighbors of vertex v as in Iterable.
+    /**
+     * Determines if it has edge.
+     *
+     * @param      v     { parameter_description }
+     * @param      w     { parameter_description }
+     *
+     * @return     True if has edge, False otherwise.
      */
-    public Iterable<Integer> adj(int v) {
-        return adj[v];
+    public boolean hasEdge(final int v, final int w) {
+        return (adj[v][w]);
     }
-
-
-   /**
-     * Return a string representation of the graph.
+    /**
+     *string representation of Graph - takes quadratic time.
+     *
+     * @return     String representation of the object.
      */
     public String toString() {
         StringBuilder s = new StringBuilder();
-        String NEWLINE = System.getProperty("line.separator");
-        s.append(V + " vertices, " + E + " edges " + NEWLINE);
-        for (int v = 0; v < V; v++) {
+        s.append(vertices + " " + edges + "\n");
+        for (int v = 0; v < vertices; v++) {
             s.append(v + ": ");
-            for (int w : adj[v]) {
+            for (int w : adj(v)) {
                 s.append(w + " ");
             }
-            s.append(NEWLINE);
+            s.append("\n");
         }
         return s.toString();
     }
