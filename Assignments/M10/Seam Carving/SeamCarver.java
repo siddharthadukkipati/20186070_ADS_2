50123fdd9c6f6966784ec10d8905e726bbf03df5
Siddhartha Dukkipati <siddharthadukipati@msitprogram.net>
2018-11-08 12:22:51 +0530

    "ads2_m10_a1_1: 0/9 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ Assignments/M10/Seam Carving/SeamCarver.java
@@ -0,0 +1,49 @@
+import java.awt.Color;
+public class SeamCarver {
+	// create a seam carver object based on the given picture
+	private Picture picture;
+	public SeamCarver(Picture picture1) {
+		if (picture == null) {
+			throw new IllegalArgumentException("Picture is null");
+		}
+		this.picture = picture1;
+	}
+	// current picture
+	public Picture picture() {
+		return picture;
+	}
+	// width of current picture
+	public int width() {
+		return picture.width();
+	}
+
+	// height of current picture
+	public int height() {
+		return picture.height();
+	}
+
+	// energy of pixel at column x and row y
+	public double energy(int x, int y) {
+		return 0;
+	}
+
+	// sequence of indices for horizontal seam
+	public int[] findHorizontalSeam() {
+		return new int[0];
+	}
+
+	// sequence of indices for vertical seam
+	public int[] findVerticalSeam() {
+		return new int[0];
+	}
+
+	// remove horizontal seam from current picture
+	public void removeHorizontalSeam(int[] seam) {
+
+	}
+
+	// remove vertical seam from current picture
+	public void removeVerticalSeam(int[] seam) {
+
+	}
+}
