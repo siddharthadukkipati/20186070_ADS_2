9b38b4fd981a481ca2dcb9dc4d4514588cb7a9a9
Siddhartha Dukkipati <siddharthadukipati@msitprogram.net>
2018-11-10 10:59:53 +0530

    "ads2_m12_a1_1: 0/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ Assignments/M12/ADS-2_Exam-2/DijkstraUndirectedSP.java
@@ -0,0 +1,199 @@
+/**
+ * Class for dijkstra undirected sp.
+ */
+public class DijkstraUndirectedSP {
+    /**
+     * distTo[v] = distance  of shortest s->v path.
+     */
+    private double[] distTo;
+    /**
+     * edgeTo[v] = last edge on shortest s->v path.
+     */
+    private Edge[] edgeTo;
+    /**
+     * priority queue of vertices.
+     */
+    private IndexMinPQ<Double> pq;
+
+    /**
+     * Computes a shortest-paths tree from the source vertex {@code s} to every
+     * other vertex in the edge-weighted graph {@code G}.
+     *
+     * @param  graph the edge-weighted digraph
+     * @param  s the source vertex
+     * @throws IllegalArgumentException if an edge weight is negative
+     * @throws IllegalArgumentException unless {@code 0 <= s < V}
+     */
+    public DijkstraUndirectedSP(final EdgeWeightedGraph graph, final int s) {
+        for (Edge e : graph.edges()) {
+            if (e.weight() < 0) {
+                throw new IllegalArgumentException(
+                    "edge " + e + " has negative weight");
+            }
+        }
+
+        distTo = new double[graph.vertices()];
+        edgeTo = new Edge[graph.vertices()];
+
+        for (int v = 0; v < graph.vertices(); v++) {
+            distTo[v] = Double.POSITIVE_INFINITY;
+        }
+        distTo[s] = 0.0;
+
+        // relax vertices in order of distance from s
+        pq = new IndexMinPQ<Double>(graph.vertices());
+        pq.insert(s, distTo[s]);
+        while (!pq.isEmpty()) {
+            int v = pq.delMin();
+            for (Edge e : graph.adj(v)) {
+                relax(e, v);
+            }
+        }
+
+        // check optimality conditions
+        assert check(graph, s);
+    }
+
+    /**
+     * relax edge e and update pq if changed.
+     *
+     * @param      e     { parameter_description }
+     * @param      v     { parameter_description }
+     */
+    private void relax(final Edge e, final int v) {
+        int w = e.other(v);
+        if (distTo[w] > distTo[v] + e.weight()) {
+            distTo[w] = distTo[v] + e.weight();
+            edgeTo[w] = e;
+            if (pq.contains(w)) {
+                pq.decreaseKey(w, distTo[w]);
+            } else {
+                pq.insert(w, distTo[w]);
+            }
+        }
+    }
+
+    /**
+     * Returns the length of a shortest path between
+     * the source vertex {@code s} and
+     * vertex {@code v}.
+     *
+     * @param  v the destination vertex
+     * @return the length of a shortest path
+     * between the source vertex {@code s} and
+     *         the vertex {@code v}; {@code Double.POSITIVE_INFINITY}
+     *         if no such path
+     * @throws IllegalArgumentException unless {@code 0 <= v < V}
+     */
+    public double distTo(final int v) {
+        return distTo[v];
+    }
+
+    /**
+     * Returns true if there is a path between the source vertex {@code s} and
+     * vertex {@code v}.
+     *
+     * @param  v the destination vertex
+     * @return {@code true} if there is a path between the source vertex
+     *         {@code s} to vertex {@code v}; {@code false} otherwise
+     * @throws IllegalArgumentException unless {@code 0 <= v < V}
+     */
+    public boolean hasPathTo(final int v) {
+        return distTo[v] < Double.POSITIVE_INFINITY;
+    }
+
+    /**
+     * Returns a shortest path between the source vertex {@code s}
+     * and vertex {@code v}.
+     *
+     * @param  v the destination vertex
+     * @return a shortest path between the source vertex {@code s}
+     *  and vertex {@code v};
+     *         {@code null} if no such path
+     * @throws IllegalArgumentException unless {@code 0 <= v < V}
+     */
+    public Iterable<Edge> pathTo(final int v) {
+        if (!hasPathTo(v)) {
+            return null;
+        }
+        Stack<Edge> path = new Stack<Edge>();
+        int x = v;
+        for (Edge e = edgeTo[v]; e != null; e = edgeTo[x]) {
+            path.push(e);
+            x = e.other(x);
+        }
+        return path;
+    }
+
+
+    // check optimality conditions:
+    // (i) for all edges e = v-w:            distTo[w] <= distTo[v] + e.weight()
+    // (ii) for all edge e = v-w on the SPT: distTo[w] == distTo[v] + e.weight()
+
+    /**
+     * check.
+     *
+     * @param      graph  The graph
+     * @param      s      { parameter_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
+    private boolean check(final EdgeWeightedGraph graph, final int s) {
+
+        // check that edge weights are nonnegative
+        for (Edge e : graph.edges()) {
+            if (e.weight() < 0) {
+                System.err.println("negative edge weight detected");
+                return false;
+            }
+        }
+
+        // check that distTo[v] and edgeTo[v] are consistent
+        if (distTo[s] != 0.0 || edgeTo[s] != null) {
+            System.err.println("distTo[s] and edgeTo[s] inconsistent");
+            return false;
+        }
+        for (int v = 0; v < graph.vertices(); v++) {
+            if (v == s) {
+                continue;
+            }
+            if (edgeTo[v] == null && distTo[v] != Double.POSITIVE_INFINITY) {
+                System.err.println("distTo[] and edgeTo[] inconsistent");
+                return false;
+            }
+        }
+
+        // check that all edges e = v-w satisfy distTo[w] <=
+        // distTo[v] + e.weight()
+        for (int v = 0; v < graph.vertices(); v++) {
+            for (Edge e : graph.adj(v)) {
+                int w = e.other(v);
+                if (distTo[v] + e.weight() < distTo[w]) {
+                    System.err.println("edge " + e + " not relaxed");
+                    return false;
+                }
+            }
+        }
+
+        // check that all edges e =
+        // v-w on SPT satisfy distTo[w] == distTo[v] + e.weight()
+        for (int w = 0; w < graph.vertices(); w++) {
+            if (edgeTo[w] == null) {
+                continue;
+            }
+            Edge e = edgeTo[w];
+            if (w != e.either() && w != e.other(e.either())) {
+                return false;
+            }
+            int v = e.other(w);
+            if (distTo[v] + e.weight() != distTo[w]) {
+                System.err.println(
+                    "edge " + e + " on shortest path not tight");
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+}
---
+++ Assignments/M12/ADS-2_Exam-2/Solution.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+public class Solution {
+
+	public static void main(String[] args) {
+		// Self loops are not allowed...
+		// Parallel Edges are allowed...
+		// Take the Graph input here...
+		// Scanner scan = new Scanner(System.in);
+		int vertexCount = Integer.parseInt(StdIn.readLine());
+		Digraph digraphObj = new Digraph(vertexCount); 
+		for (int i = 0; i < vertexCount; i++) {
+			String[] edges = StdIn.readLine().split(" ");
+			for (int k = 1; k < edges.length ; k++) {
+				digraphObj.addEdge(Integer.parseInt(edges[0]), Integer.parseInt(edges[k]));
+			}
+		}
+		String caseToGo = StdIn.readLine();
+		switch (caseToGo) {
+		case "Graph":
+			//Print the Graph Object.
+			System.out.println(digraphObj);
+
+			break;
+
+		case "DirectedPaths":
+			// Handle the case of DirectedPaths, where two integers are given.
+			// First is the source and second is the destination.
+			// If the path exists print the distance between them.
+			// Other wise print "No Path Found."
+			break;
+
+		case "ViaPaths":
+			// Handle the case of ViaPaths, where three integers are given.
+			// First is the source and second is the via is the one where path should pass throuh.
+			// third is the destination.
+			// If the path exists print the distance between them.
+			// Other wise print "No Path Found."
+			break;
+
+		default:
+			break;
+		}
+
+	}
+}
